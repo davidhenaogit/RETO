2cabfc3ec541a00b4469c89b2db222e0dce94655
Juan David <juanhen@gmail.com>
2023-05-19 11:05:39 -0500

    hola prueba react

---
+++ React/separador/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Palabra p =new Palabra();
+
+        p.ingresarFrase();
+
+        p.recorrerPalabra();
+
+        p.invertirPalabra();
+
+        p.numeroIntermedio();
+
+      //  p.mostarcaracteresYpalabras();
+      
+    }
+}
